12c87794fe64a521d5c92330306cbcd80a415301
jisung <jisung.smart@gmail.com>
2020-01-18 06:34:33 +0900

    Initial Commit

---
+++ YouTubeDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YouTubeDemo
+//
+//  Created by Hae kyeom Kim on 2020/01/18.
+//  Copyright © 2020 Hae kyeom Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
